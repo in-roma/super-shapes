3c00e6c07316b774744d0f8a086af6300c8418b5
in-roma <romain.grandjean@outlook.com>
2021-02-01 12:26:28 +0100

    animations input

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+/* 1st Column  */
+/* Top frame */
+const movingRightToLeft = anime({
+	targets: '.conveyor',
+	translateX: '-50%',
+	loop: true,
+	duration: 2000,
+	easing: 'linear',
+});
+
+/* 2nd Column  */
+/* Bottom frame */
 const scaling = anime({
 	targets: '#tunnel circle',
 	scale: 1.1,
@@ -5,4 +17,23 @@ const scaling = anime({
 	loop: true,
 	duration: 450,
 	easing: 'easeInOutSine',
+	delay: (el, i) => i * 100,
+});
+
+/* 3rd Column  */
+/* Top frame */
+
+const pathEl = document.querySelector('#zigzag path');
+const offset = anime.setDashoffset(pathEl);
+
+pathEl.setAttribute('stroke-dashoffset', offset);
+anime({
+	targets: pathEl,
+	strokeDashoffset: [offset, 0],
+	duration: anime.random(1000, 3000),
+	delay: anime.random(0, 2000),
+	loop: true,
+	direction: 'alternate',
+	easing: 'easeInOutSine',
+	autoplay: true,
 });
